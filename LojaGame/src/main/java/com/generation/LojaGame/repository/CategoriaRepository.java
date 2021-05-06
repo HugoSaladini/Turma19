9598421a69d1be16eb4bdfe16eab7a2e9d3b310a
HugoSaladini <hugo.saladini@gmail.com>
2021-05-06 13:48:12 -0300

    UP

---
+++ LojaGame/src/main/java/com/generation/LojaGame/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.LojaGame.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.LojaGame.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
