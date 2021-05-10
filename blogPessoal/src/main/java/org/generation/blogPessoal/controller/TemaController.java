b7c4eefe2fb075c96f45e4c55c6b4cc17546f7fa
HugoSaladini <hugo.saladini@gmail.com>
2021-05-10 12:06:31 -0300

    atualizacao

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -1,4 +1,4 @@
-package com.generation.LojaGame.controller;
+package org.generation.blogPessoal.controller;
 
 import java.util.List;
 
@@ -15,47 +15,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.generation.LojaGame.model.Produto;
-import com.generation.LojaGame.repository.ProdutoRepository;
+import org.generation.blogPessoal.model.Tema;
+import org.generation.blogPessoal.repository.TemaRepository;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping("/produto")
-public class ProdutoController {
+@RequestMapping("/tema")
+public class TemaController {
 	
 	@Autowired
-	private ProdutoRepository repository;
-
+	private TemaRepository repository;
+	
 	@GetMapping
-	public ResponseEntity<List<Produto>> getAll() {
-
+	public ResponseEntity<List<Tema>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
-
 	}
-
+	
 	@GetMapping("/{id}")
-	public ResponseEntity<Produto> getById(@PathVariable long id) {
-
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
-
+	public ResponseEntity<Tema> getById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
-
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Tema>> getByName(@PathVariable String nome){
+		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(nome));
+	}
+	
 	@PostMapping
-	private ResponseEntity<Produto> post(@RequestBody Produto produto) {
-
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	public ResponseEntity<Tema> post (@RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(tema));
 	}
 
 	@PutMapping
-	public ResponseEntity<Produto> put(@RequestBody Produto produto) {
-
-		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
-
+	public ResponseEntity<Tema> put (@RequestBody Tema tema){
+		return ResponseEntity.ok(repository.save(tema));				
 	}
-
+	
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
-
 		repository.deleteById(id);
 	}
 	
---
+++ blogPessoal/src/main/java/org/springframework/security/config/annotation/authentication/builders/AuthenticationManagerBuilder.java
@@ -0,0 +1,12 @@
+package org.springframework.security.config.annotation.authentication.builders;
+
+import org.generation.blogPessoal.seguranca.UserDetailsServiceImpl;
+
+public class AuthenticationManagerBuilder {
+
+	public void userDetailsService(UserDetailsServiceImpl userDetailsService) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
